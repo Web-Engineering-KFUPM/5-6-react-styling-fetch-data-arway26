2cea705d4bd6a315078ff14ed34754ddb44d0541
Arwa <s202242580@kfupm.edu.sa>
2025-10-15 11:51:16 +0300

    Task3 done

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -324,17 +324,52 @@ import UserModal from './components/UserModal'
 
 function App() {
   const [users, setUsers] = useState([])
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [showModal, setShowModal] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(null)
   
 
   useEffect(() => {
-    {/*API fetch logic*/}
+    const fetchUsers = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setUsers(data)
+      setFilteredUsers(data)
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  
+  fetchUsers()
 
   }, [])
 
+  useEffect(() => {
+  if (searchTerm === '') {
+    setFilteredUsers(users)
+  } else {
+    const filtered = users.filter(user =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setFilteredUsers(filtered)
+  }
+}, [searchTerm, users])
+
   const handleUserClick = (user) => {
+   setSelectedUser(user)
+   setShowModal(true)
   }
 
   const handleCloseModal = () => {
+   setShowModal(false)
+   setSelectedUser(null)
   }
 
   return (
@@ -348,13 +383,13 @@ function App() {
       
 
       <Container className="py-3 mb-4">
-        <SearchBar />
+        <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
 
         {/* {loading && <Spinner ... />} */}
         {/* {error && <Alert ...>{error}</Alert>} */}
-        {/* <UserList users={filteredUsers} onUserClick={handleUserClick} /> */}
+        <UserList users={filteredUsers} onUserClick={handleUserClick} />
 
-        <UserModal />
+        <UserModal show={showModal} user={selectedUser} onHide={handleCloseModal}/>
       </Container>
 
       <footer className="bg-light py-4 mt-5">
